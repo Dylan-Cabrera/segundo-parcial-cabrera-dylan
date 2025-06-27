7848e3852a8d93bff3b31d96ade0814b76ebc5b9
dylanC <cabreradylan055@gmail.com>
2025-06-27 12:53:28 -0300

    ruta para actualizar movie

--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
 
-import { allMovies, movieID, createMovie } from "../controllers/movie.controllers.js";
+import { allMovies, movieID, createMovie, updateMovie } from "../controllers/movie.controllers.js";
 
 router.get('/movies', allMovies);
 router.get('/movies:id', movieID);
 router.post('/movies', createMovie);
+router.put('/movies:id', updateMovie);
 
 export default router;
